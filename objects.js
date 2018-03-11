c029265557135fb3da1fd8850ce5a8bdb01bc8b2
Kevin Nwoke <kevinnwoke@yahoo.com>
2018-03-11 20:01:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = { artistName: 'songTitle' };
 
 function updatePlaylist (playlist, artistName, songTitle){
+<<<<<<< HEAD
   playlist['Phil Ochs'] = "Here's to the State of Mississippi";
   return playlist;
 };
@@ -9,3 +10,10 @@ function removeFromPlaylist (playlist, artistName){
   delete playlist["Kanye"];
   return playlist;
 }
+=======
+  playlist.artistName = 'songTitle';
+  return playlist
+};
+
+updatePlaylist(playlist, "Phil Ochs", "Here's to the State of Mississippi");
+>>>>>>> ede601b4ba964e2b4bf94aef50d18159e950df33
